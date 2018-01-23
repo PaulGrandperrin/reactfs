d57c2e7466b645028aecaa276bbf5d4bd9dc93f2
Paul Grandperrin <paul.grandperrin@gmail.com>
2018-01-23 14:06:44 +0100

    tests: reactor: check that the computed fibonacci number is correct

--- src/reactor/tests.rs
+++ src/reactor/tests.rs
@@ -14,7 +14,7 @@ use super::*;
 
 proptest! {
 	#[test]
-	fn fibonacci_seq(size in 0u64..20) {
+	fn fibonacci_seq(n in 0u64..20) {
 	    let (bd_sender, bd_receiver) = channel::<BDRequest>();
 	    let (fs_sender, _fs_receiver) = channel::<FSResponse>();
 	    let (react_sender, react_receiver) = channel::<Event>();
@@ -27,14 +27,21 @@ proptest! {
 	    let mut core = Core::new(bd_sender, fs_sender, react_receiver);
 	    let handle = core.handle();
 	    
-	    let f = write_fibonacci_seq(handle, size);
+        let f = write_fibonacci_seq(handle.clone(), n)
+            .and_then(|_|{
+                read_u64(&handle, n)
+            })
+            .and_then(|r|{
+                assert!(r == fibonacci(n));
+                Ok(())
+            });
 
 	    let r = core.run(f);
 	    assert!(r.is_ok());
 	}
 
 	#[test]
-	fn fibonacci_rec(size in 0u64..20) {
+	fn fibonacci_rec(n in 0u64..20) {
 	    let (bd_sender, bd_receiver) = channel::<BDRequest>();
 	    let (fs_sender, _fs_receiver) = channel::<FSResponse>();
 	    let (react_sender, react_receiver) = channel::<Event>();
@@ -47,7 +54,14 @@ proptest! {
 	    let mut core = Core::new(bd_sender, fs_sender, react_receiver);
 	    let handle = core.handle();
 	    
-	    let f = write_fibonacci_rec(handle, size as u64);
+	    let f = write_fibonacci_rec(handle.clone(), n)
+            .and_then(|_|{
+                read_u64(&handle, n)
+            })
+            .and_then(|r|{
+                assert!(r == fibonacci(n));
+                Ok(())
+            });
 
 	    let r = core.run(f);
 	    assert!(r.is_ok());
@@ -74,14 +88,30 @@ fn error_propagation() {
     let r = core.run(f);
     assert!(r.is_err());
 }
+
+fn fibonacci(n: u64) -> u64 {
+    match n {
+        0 | 1 => n,
+        _ => {
+            let mut pred = 0;
+            let mut curr = 1;
+            for _ in 1..n {
+                let tmp = curr;
+                curr += pred;
+                pred = tmp;
+            }
+            curr
+        }
+    }
+}
     
-fn _write_u64(h: &Handle, n: u64, offset: u64) -> FutureWrite {
+fn write_u64(h: &Handle, n: u64, offset: u64) -> FutureWrite {
     let mut v = vec![0;8];
     byteorder::BigEndian::write_u64(&mut v, n);
     h.write(v, offset*8)
 }
 
-fn _read_u64(h: &Handle, offset: u64) -> impl Future<Item=u64, Error=failure::Error> {
+fn read_u64(h: &Handle, offset: u64) -> impl Future<Item=u64, Error=failure::Error> {
     h.read(offset*8, 8).map(|v| {
         byteorder::BigEndian::read_u64(&v)
     })
@@ -92,19 +122,19 @@ fn write_fibonacci_rec(h:Handle, n: u64) -> Result<(), failure::Error> {
     match n {
         0|1 => {
             let f = Future::join(
-                _write_u64(&h, 0, 0),
-                _write_u64(&h, 1, 1)
+                write_u64(&h, 0, 0),
+                write_u64(&h, 1, 1)
                 );
             await!(f)?;
         },
         _ => {
             await!(write_fibonacci_rec(h.clone(), n-1))?;
             let f = Future::join(
-                _read_u64(&h, n-1),
-                _read_u64(&h, n-2)
+                read_u64(&h, n-1),
+                read_u64(&h, n-2)
             );
             let r = await!(f)?;
-            await!(_write_u64(&h, r.0+r.1, n))?;
+            await!(write_u64(&h, r.0+r.1, n))?;
         }
     }
     Ok(())
@@ -113,23 +143,23 @@ fn write_fibonacci_rec(h:Handle, n: u64) -> Result<(), failure::Error> {
 #[async]
 fn write_fibonacci_seq(h:Handle, n: u64) -> Result<(), failure::Error> {
     let f = Future::join(
-        _write_u64(&h, 0, 0),
-        _write_u64(&h, 1, 1)
+        write_u64(&h, 0, 0),
+        write_u64(&h, 1, 1)
         );
     await!(f)?;
 
     for i in 2..n {
         let f = Future::join(
-            _read_u64(&h, i-1),
-            _read_u64(&h, i-2)
+            read_u64(&h, i-1),
+            read_u64(&h, i-2)
         );
         let r = await!(f)?;
-        await!(_write_u64(&h, r.0+r.1, i))?;
+        await!(write_u64(&h, r.0+r.1, i))?;
     }
     Ok(())
 }
 
 #[async]
 fn read_error(h:Handle) -> Result<u64, failure::Error> {
-    await!(_read_u64(&h, 1000000000))
+    await!(read_u64(&h, 1000000000))
 }
