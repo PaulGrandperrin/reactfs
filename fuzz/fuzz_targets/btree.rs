b43fad0b078b89a73903aa2e1ec7bbace12aa4dd
Paul Grandperrin <paul.grandperrin@gmail.com>
2018-02-12 20:15:49 +0100

    fuzzing: move from LLVM's LibFuzzer to Google's Honggfuzz

--- fuzz/fuzz_targets/btree.rs
+++
@@ -1,9 +0,0 @@
-#![no_main]
-#[macro_use] extern crate libfuzzer_sys;
-extern crate reactfs;
-
-use reactfs::core::instrumentation::*;
-
-fuzz_target!(|data: &[u8]| {
-   fuzz_btree(data);
-});
---
+++ hfuzz/btree.rs
@@ -0,0 +1,18 @@
+extern crate reactfs;
+#[macro_use] extern crate honggfuzz;
+
+#[cfg(all(fuzzing))]
+use reactfs::core::instrumentation::*;
+
+fn main() {
+    loop {
+        fuzz!(|data: &[u8]| {
+        	// we don't want to explode the search space
+    		if data.len() > 100 * (8 + 8) {return}
+
+    		let mut vec = raw_to_vec_of_tuple_u64(data);
+
+            insert_checked(vec);
+        });
+    }
+}
