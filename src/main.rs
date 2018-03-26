ec0e3ccdcb29b1c9839c01accb3c9b75591ccc77
Paul Grandperrin <paul.grandperrin@gmail.com>
2018-03-26 16:40:02 +0200

    main.rs: add boilerplate main() with error handling

--- src/main.rs
+++ src/main.rs
@@ -36,9 +36,47 @@ pub mod reactor;
 pub mod backend;
 pub mod core;
 
+// we use by default the Error implementation from failure's crate
+pub type Result<T> = std::result::Result<T, failure::Error>;
+
 fn main() {
+    // launch start function and print potential errors
+    match start() {
+        Ok(_)  => {}
+        Err(e) => {
+            eprintln!("Execution failed with error: {}", e);
+
+            let print_backtrace = std::env::var("RUST_BACKTRACE").unwrap_or("0".into()) != "0";
+
+            if print_backtrace {
+                eprintln!("{}", e.backtrace());
+            }
+
+            // print chain of causes
+            for f in e.causes().skip(1) {
+                eprintln!("caused by: {}", f);
+
+                if print_backtrace {
+                    if let Some(b) = f.backtrace() {
+                        eprintln!("{}", b);
+                    }
+                }
+            }
+
+            if !print_backtrace {
+                eprintln!("note: Run with `RUST_BACKTRACE=1` for a backtrace.");
+            }
+
+            std::process::exit(101);
+        }
+    }
+}
+
+fn start() -> Result<(), failure::Error> {
     let vec = vec![(3,30), (1,10),(2,20), (4,40), (5,50),
                    (6,60), (7,70)];
     
     core::instrumentation::insert_checked(vec);
+
+    Ok(())
 }
